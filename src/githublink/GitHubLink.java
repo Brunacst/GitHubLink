f32def8ed7bdffdbcbf47afddbb6dee6f18ac65f
Brunacst <2024148@student.cct.ie>
2026-05-05 10:18:12 +0100

    Added Scanner plus import for Scanner library

--- src/githublink/GitHubLink.java
+++ src/githublink/GitHubLink.java
@@ -4,6 +4,8 @@
  */
 package githublink;
 
+import java.util.Scanner;
+
 /**
  *
  * @author bruna
@@ -15,6 +17,8 @@ public class GitHubLink {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner myKB = new Scanner(System.in);
     }
     
 }
